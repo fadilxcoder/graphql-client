29fe94d891f8b0852b242b2665213bb3ce9a195e
fadilxcoder <rosunmungurudm@gmail.com>
2021-04-07 18:33:30 +0000

    multiple query & depth level

--- php/packagist.php
+++ php/packagist.php
@@ -14,41 +14,57 @@ dump("Tracy Active !");
 $client = new Client(
     "https://graphqlzero.almansi.me/api"
 );
-
-$gql = (new Query('user'))
-    ->setArguments(['id' => 3])
+    
+$gql = (new Query())
     ->setSelectionSet(
         [
-            'id',
-            'email',
-            'name',
-            'address{street}',
-            (new Query('posts'))
-                ->setSelectionSet(
-                    [
-                        'data{title}',
-                    ]
-                )
+            (new Query('user'))
+            ->setArguments(['id' => 3])
+            ->setSelectionSet(
+                [
+                    'id',
+                    'email',
+                    'address{street}',
+                    (new Query('posts'))
+                    ->setSelectionSet(
+                        [
+                            'data{id}',
+                            'data{comments{data{email}}}',
+                        ]
+                    )
+                ]
+            ),
+            (new Query('post'))
+            ->setArguments(['id' => 5])
+            ->setSelectionSet(
+                [
+                    'id',
+                    'title',
+                ]
+            )
         ]
     )
-    ;
+;    
 
 
 try {
-    $results = $client->runQuery($gql);
-}
-catch (QueryError $exception) {
-
-    dump($exception->getErrorDetails());
+    $results = $client->runQuery($gql);     # Run query to get results
+    
+    // dump($results->getResponseObject());    # Display original response from endpoint
+    
+    // dump($results->getData()->user);        # Display part of the returned results of the object
+    
+    // $results->reformatResults(true);
+    // dump($results->getData()['user']);      # Reformat the results to an array and get the results of part of the array
+    
+    /* logic handler */
+    
+    dump($results->getData()->user);
+    dump($results->getData()->post);
+    
+} catch (QueryError $exception) {
+    dump($exception->getErrorDetails());    # Catch query error and desplay error details
     exit;
 }
 
 
-dump($results->getResponseObject());
-
-
-dump($results->getData()->user);
-
-
-$results->reformatResults(true);
-dump($results->getData()['user']);
